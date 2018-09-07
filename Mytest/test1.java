f1d8cf205f1bf7c0f4b3ff51b6912260d7c14c4b
HuXingkai <https://HuXingkai@github.com>
2018-09-07 22:19:57 +0800

    StringBuffer test

--- Mytest/test1.java
+++ Mytest/test1.java
@@ -7,5 +7,13 @@ public class test1 {
     public static void main(String[] args) {
         double d=3.1415926;
         System.out.printf("%.2f",d);
+
+        StringBuffer sb3=new StringBuffer("a");
+        System.out.println("length="+sb3.length());//length=1
+        System.out.println("capacity="+sb3.capacity());//capacity=17;
+        System.out.println("--------------------");
+        sb3.append("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("length="+sb3.length());//length=48;
+        System.out.println("capacity="+sb3.capacity());//capacity=48;
     }
 }
